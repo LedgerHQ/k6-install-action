d6d2309f2ce8b91312934a9d8e77114d28b953a8
Ivan Sandratskyi <ivan.sandratskiy-ext@ledger.fr>
2021-04-27 18:11:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const { execSync } = require('child_process')
 
 console.log(`INSTALLING K6`);
 
-execSync('sudo yum update', {stdio: 'inherit'})
-execSync('sudo yum -y install https://dl.k6.io/rpm/repo.rpm', {stdio: 'inherit'})
+execSync('sudo yum install https://dl.k6.io/rpm/repo.rpm', {stdio: 'inherit'})
 execSync('sudo yum -y install k6', {stdio: 'inherit'})
 
 console.log(`K6 INSTALLED SUCCESSFULLY`);
