84ee846256a9f122422f890f34f2473a22b35b4c
Ivan Sandratskiy <ivan.sandratskiy-ext@ledger.fr>
2021-04-27 17:00:35 +0300

    v1.2.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process')
 
 console.log(`INSTALLING K6`);
 
-execSync('sudo dnf install https://dl.k6.io/rpm/repo.rpm', {stdio: 'inherit'})
-execSync('sudo dnf install k6', {stdio: 'inherit'})
+execSync('sudo yum install https://dl.k6.io/rpm/repo.rpm', {stdio: 'inherit'})
+execSync('sudo yum install k6', {stdio: 'inherit'})
 
 console.log(`K6 INSTALLED SUCCESSFULLY`);
